b4725748cbe70408bd7c0c4e55832be209ecd7a9
kjy <spacejay1007@gmail.com>
2024-05-03 15:47:54 +0900

    divisorExtraction algorithm && knightwepon algorithm

--- ProgrammersTest/Lv0/DivisorExtraction/DivisorExtraction.js
+++ ProgrammersTest/Lv0/DivisorExtraction/DivisorExtraction.js
@@ -12,33 +12,34 @@ function DivisorExtraction(num) {
 // console.log(DivisorExtraction(8));
 
 // 1
-function solutionOne(n) {
-  console.log(
-    Array(n)
-      .fill(0)
-      .map((v, index) => {
-        console.log(v, index, v + index);
-      }).reduce(())
-  );
+// function solutionOne(n) {
+//   console.log(
+//     Array(n)
+//       .fill(0)
+//       .map((v, index) => {
+//         console.log(v, index, v + index);
+//       }).reduce(())
+//   );
 
-  return Array(n)
-    .fill(0)
-    .map((v, index) => v + index + 1)
-    .filter((v) => n % v === 0).length;
-}
+//   return Array(n)
+//     .fill(0)
+//     .map((v, index) => v + index + 1)
+//     .filter((v) => n % v === 0).length;
+// }
 
-console.log(solutionOne(8));
+// console.log(solutionOne(8));
 
-// 2
-function solutionTwo(n) {
-  let s = new Set();
-  for (let i = 1; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) {
-      s.add(i);
-      s.add(n / i);
-    }
-  }
+// // 2
+// function solutionTwo(n) {
+//   let s = new Set();
+//   for (let i = 1; i <= Math.sqrt(n); i++) {
+//     if (n % i === 0) {
+//       s.add(i);
+//       s.add(n / i);
+//     }
+//   }
 
-  return [...s].sort((a, b) => a - b);
-}
+//   return [...s].sort((a, b) => a - b);
+// }
 
+console.log(DivisorExtraction(35));
--- ProgrammersTest/Lv1/AKnightsWeapon/AKnightsWeapon.js
+++ ProgrammersTest/Lv1/AKnightsWeapon/AKnightsWeapon.js
@@ -1,8 +1,10 @@
 // 기사 단원의 무기
 
+// 내가 작성한 알고리즘 > 시간 초과가 몇개 나옴
 function solution(number, limit, power) {
   var answer = 0;
   const att = [];
+
   for (let i = 1; i <= number; i++) {
     const newArr = Array(i)
       .fill(1)
@@ -17,4 +19,65 @@ function solution(number, limit, power) {
   }, 0);
 
   return attPlus;
+}
+
+// 1
+function solution1(number, limit, power) {
+  let answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let divisor = 0;
+    for (let j = 1; j <= Math.sqrt(i); j++) {
+      if (i % j === 0) {
+        if (i / j === j) divisor += 1;
+        else divisor += 2;
+      }
+      if (divisor > limit) {
+        divisor = power;
+        break;
+      }
+    }
+    answer += divisor;
+  }
+
+  return answer;
+}
+
+// 2
+
+const sol = (n) => {
+  if (n == 1) return 1;
+
+  let ret = 1;
+
+  for (let i = 1; i <= n / 2; i++) if (n % i == 0) ret++;
+
+  return ret;
+};
+
+function solution2(number, limit, power) {
+  let ans = 0;
+
+  for (let i = 1; i <= number; i++) {
+    const cnt = sol(i);
+    ans += cnt > limit ? power : cnt;
+  }
+
+  return ans;
+}
+
+// 3
+
+function solution3(number, limit, power) {
+  const sum = (num) => {
+    let res = 0;
+    for (let i = 1; i * i <= num; i++) {
+      if (i * i === num) res++;
+      else if (num % i === 0) res += 2;
+    }
+    return res;
+  };
+  return Array.from({ length: number }, (el, i) => i + 1)
+    .map((el) => (sum(el) > limit ? power : sum(el)))
+    .reduce((acc, cur) => acc + cur, 0);
 }
